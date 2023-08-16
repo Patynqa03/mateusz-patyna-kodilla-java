3fc87000bb24da12900cc8ceae8900cb6786632f
Mateusz Patyna <patynamateusz66@gmial.com>
2023-08-16 12:24:04 +0200

    14.1 Spring

---
+++ kodilla-spring/src/main/java/com/kodilla/spring/forum/ForumUser.java
@@ -0,0 +1,23 @@
+package com.kodilla.spring.forum;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ForumUser {
+
+     String username;
+
+    public ForumUser(String username) {
+
+        this.username = "John Smith";
+
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
