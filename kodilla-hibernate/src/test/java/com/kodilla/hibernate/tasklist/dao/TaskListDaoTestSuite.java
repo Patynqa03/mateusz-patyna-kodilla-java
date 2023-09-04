1af87a0d2aa54d6a0dbe40d7fb1305f4fc199117
Mateusz Patyna <patynamateusz66@gmial.com>
2023-09-05 00:25:05 +0200

    17.2

---
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/tasklist/dao/TaskListDaoTestSuite.java
@@ -0,0 +1,34 @@
+package com.kodilla.hibernate.tasklist.dao;
+
+import com.kodilla.hibernate.tasklist.TaskList;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+
+@SpringBootTest
+public class TaskListDaoTestSuite {
+
+@Autowired
+    private TaskListDao taskListDao;
+    private static final String DESCRIPTION = "Test";
+
+    @Test
+    void testFindByListName() {
+        TaskList taskList = new TaskList("LISTA",DESCRIPTION);
+        taskListDao.save(taskList);
+
+        String taskListTempValue = taskList.getListName();
+
+        List<TaskList> readTaskList = taskListDao.findByListName(taskListTempValue);
+
+        assertEquals(1,readTaskList.size());
+
+        int id = readTaskList.get(0).getId();
+        taskListDao.deleteById(id);
+
+    }
+}
