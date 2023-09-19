e5554ebca1b0f456c51f8fb3fa6fb8e601534902
Mateusz Patyna <patynamateusz66@gmial.com>
2023-09-19 16:41:08 +0200

    poprawki

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/manytomany/dao/EmployeeDao.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/manytomany/dao/EmployeeDao.java
@@ -2,6 +2,7 @@ package com.kodilla.hibernate.manytomany.dao;
 
 import com.kodilla.hibernate.manytomany.Employee;
 import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,5 +12,6 @@ import java.util.List;
 @Transactional
 public interface EmployeeDao extends CrudRepository<Employee,Integer> {
 
+    @Query
     List<Employee> getEmployeeWithSpecificName(String lastName);
 }
